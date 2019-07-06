5d00fcad421bc0f951a8e907b7c7d6684b44cc84
jianke <1340289@qq.com>
2019-07-06 09:57:47 +0800

    update

--- restart-study/Restudy_2/src/Float.java
+++ restart-study/Restudy_2/src/Float.java
@@ -13,7 +13,7 @@ public class Float {
 		
 	}
 	public static void Test_default() {
-		int[] arr=new int[5] {1,2,3,4};
+		int[] arr=new int[] {1,2,3,4};
 		for(int i=0;i<arr.length;i++) {
 			System.out.println(arr[i]);
 		}
